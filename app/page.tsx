80f9ae4fc10865af5c8fca21929d8abd50936838
Busami Victor <victorbusami1@gmail.com>
2025-01-25 13:16:21 +0300

    forms commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 
 import Image from 'next/image'
 import PatientForm from '@/components/forms/PatientForm'
+import Link from 'next/link'
 
 const Home = () => {
   return (
@@ -9,9 +10,16 @@ const Home = () => {
         <div className="sub-container max-w-[496px]">
           <Image src='/assets/icons/logo-full.svg' height={1000} width={1000} alt='patient' className='mb-12 h-10 w-fit' />
         </div>
-      </section>
+      
 
       <PatientForm />
+      <div className='text-14-regular mt-20 flex justify-between'>
+      <p className='justify-items-end text-dark-600 xl:text-left'>©2025 apex-nova</p>
+      <Link href='/?admin=true' className='text-green-500'>Admin</Link>
+      
+      </div>
+      </section>
+      <Image src='/assets/images/onboarding-img.png' height={1000} width={1000} alt='patient' className='side-img max-w-[50%]' />
       
     </div>
   )
